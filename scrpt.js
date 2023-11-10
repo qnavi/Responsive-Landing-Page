666d9131f83276e631861692d0f460c4e3e1ea7e
ivanvillaverde <147516465+qnavi@users.noreply.github.com>
2023-11-10 18:17:17 +0800

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+    jQuery(document).ready(function ($) {
+
+        $('#checkbox').change(function(){
+          setInterval(function () {
+              moveRight();
+          }, 3000);
+        });
+        
+          var slideCount = $('#slider ul li').length;
+          var slideWidth = $('#slider ul li').width();
+          var slideHeight = $('#slider ul li').height();
+          var sliderUlWidth = slideCount * slideWidth;
+          
+          $('#slider').css({ width: slideWidth, height: slideHeight });
+          
+          $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+          
+          $('#slider ul li:last-child').prependTo('#slider ul');
+      
+          function moveLeft() {
+              $('#slider ul').animate({
+                  left: + slideWidth
+              }, 200, function () {
+                  $('#slider ul li:last-child').prependTo('#slider ul');
+                  $('#slider ul').css('down', '');
+              });
+          };
+      
+          function moveRight() {
+              $('#slider ul').animate({
+                  left: - slideWidth
+              }, 200, function () {
+                  $('#slider ul li:first-child').appendTo('#slider ul');
+                  $('#slider ul').css('down', '');
+              });
+          };
+      
+          $('a.control_prev').click(function () {
+              moveLeft();
+          });
+      
+          $('a.control_next').click(function () {
+              moveDown();
+          });
+      
+      });    
+      
+});
